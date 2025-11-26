c89a1b80d5e3d9aba6eba4866e4e7c5a426fe166
germano aquino <germano.r.aquino@gmail.com>
2025-11-26 14:40:23 -0300

    fix: correct env variables name

--- infra/database.js
+++ infra/database.js
@@ -18,20 +18,12 @@ async function query(queryObject) {
 }
 
 function getNewClient() {
-  console.log({
-    host: process.env.POSTGRESS_HOST,
-    port: process.env.POSTGRESS_PORT,
-    user: process.env.POSTGRESS_USER,
-    database: process.env.POSTGRESS_DB,
-    password: process.env.POSTGRESS_PASSWORD,
-  });
-
   const client = new Client({
-    host: process.env.POSTGRESS_HOST,
-    port: process.env.POSTGRESS_PORT,
-    user: process.env.POSTGRESS_USER,
-    database: process.env.POSTGRESS_DB,
-    password: process.env.POSTGRESS_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
 
   return client;
