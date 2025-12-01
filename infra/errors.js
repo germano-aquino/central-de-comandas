86e9c44cfc286d6706d51193e141e7916dbd8730
germano aquino <germano.r.aquino@gmail.com>
2025-12-01 13:20:43 -0300

    feat: add `MethodNotAllowedError` and `ServiceError`

--- infra/errors.js
+++ infra/errors.js
@@ -15,3 +15,39 @@ export class InternalServerError extends Error {
     };
   }
 }
+
+export class MethodNotAllowedError extends Error {
+  constructor({ message, cause }) {
+    super(message || "Método não permitido para este endpoint.", cause);
+    this.name = "MethodNotAllowedError";
+    this.action = "Verifique se o método HTTP é válido para este endpoint.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ message, cause }) {
+    super(message || "O serviço indisponível no momento.", cause);
+    this.name = "ServiceError";
+    this.action = "Verifique se o serviço está disponível.";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
