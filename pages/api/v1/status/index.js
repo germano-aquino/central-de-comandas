57e4036cdb8c41a736f5937b33db3f9b4776059e
germano aquino <germano.r.aquino@gmail.com>
2025-12-01 12:23:45 -0300

    feat: create and use `InternalServerError` on `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,32 +1,42 @@
+import { InternalServerError } from "infra/errors";
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const updatedAt = new Date();
+  try {
+    const updatedAt = new Date();
 
-  const versionResult = await database.query("SHOW server_version;");
-  const version = versionResult.rows[0].server_version;
+    const versionResult = await database.query("SHOW server_version;");
+    const version = versionResult.rows[0].server_version;
 
-  const maxConnectionsResult = await database.query("SHOW max_connections;");
-  const maxConnections = Number(maxConnectionsResult.rows[0].max_connections);
+    const maxConnectionsResult = await database.query("SHOW max_connections;");
+    const maxConnections = Number(maxConnectionsResult.rows[0].max_connections);
 
-  const databaseName = process.env.POSTGRES_DB;
-  const openedConnectionsResult = await database.query({
-    text: "SELECT * FROM pg_stat_activity WHERE state = 'active' AND datname=$1;",
-    values: [databaseName],
-  });
-  const openedConnections = openedConnectionsResult.rowCount;
+    const databaseName = process.env.POSTGRES_DB;
+    const openedConnectionsResult = await database.query({
+      text: "SELECT * FROM pg_stat_activity WHERE state = 'active' AND datname=$1;",
+      values: [databaseName],
+    });
+    const openedConnections = openedConnectionsResult.rowCount;
 
-  const statusBody = {
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version,
-        max_connections: maxConnections,
-        opened_connections: openedConnections,
+    const statusBody = {
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version,
+          max_connections: maxConnections,
+          opened_connections: openedConnections,
+        },
       },
-    },
-  };
-  return response.status(200).json(statusBody);
+    };
+    return response.status(200).json(statusBody);
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({ cause: error });
+
+    console.error(publicErrorObject);
+    return response
+      .status(publicErrorObject.statusCode)
+      .json(publicErrorObject);
+  }
 }
 
 export default status;
