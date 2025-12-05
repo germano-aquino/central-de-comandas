fdd550398f0ab4dcc2c43471cdc73f3ffba19552
germano aquino <germano.r.aquino@gmail.com>
2025-12-05 17:16:37 -0300

    feat: add `email` service

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,8 +5,11 @@ import migrator from "models/migrator";
 import session from "models/session";
 import user from "models/user";
 
+const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
+
 async function waitForAllServices() {
   await waitForWebServer();
+  await waitForEmailServer();
 
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
@@ -18,7 +21,22 @@ async function waitForAllServices() {
       const response = await fetch("http://localhost:3000/api/v1/status");
 
       if (response.status !== 200) {
-        throw Error();
+        throw new Error();
+      }
+    }
+  }
+
+  async function waitForEmailServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch(emailHttpUrl);
+
+      if (response.status !== 200) {
+        throw new Error();
       }
     }
   }
@@ -30,6 +48,25 @@ async function clearDatabase() {
 
 async function runPendingMigrations() {
   await migrator.runPendingMigrations();
+}
+
+async function deleteAllEmails() {
+  await fetch(`${emailHttpUrl}/messages`, {
+    method: "DELETE",
+  });
+}
+
+async function getLastEmail() {
+  const response = await fetch(`${emailHttpUrl}/messages`);
+  const emailList = await response.json();
+  const lastEmailItem = emailList.pop();
+
+  const lastEmailBody = await fetch(
+    `${emailHttpUrl}/messages/${lastEmailItem.id}.plain`,
+  );
+
+  lastEmailItem.text = await lastEmailBody.text();
+  return lastEmailItem;
 }
 
 async function createUser(userInputValues) {
@@ -52,6 +89,8 @@ const orchestrator = {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
+  deleteAllEmails,
+  getLastEmail,
   createUser,
   createSession,
 };
